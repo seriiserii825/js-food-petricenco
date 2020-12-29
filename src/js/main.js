6c94fea85d82ac13b137bb2cc3d97592a7fc1b22
serii <seriiburduja@gmail.com>
2020-12-29 18:32:29 +0200

    some

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,6 @@ import timer from "./modules/timer";
 document.addEventListener('DOMContentLoaded', () => {
 	new Tabs('.tabheader', '.tabheader__item', '.tabcontent');
 	timer();
+	const wrapper = document.querySelector('.offer__slider-wrapper');
+	console.log(getComputedStyle(wrapper).boxShadow);
 });
